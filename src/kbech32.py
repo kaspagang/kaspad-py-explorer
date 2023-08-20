72225c3e6c09114fd4145fdd76d07c74ae453741
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2023-08-20 02:20:04 -0600

    Support encoding ECDSA address in kbech32 (#4)
    - Handled setting version bit correctly
    - Also handled missing bits padding

--- src/kbech32.py
+++ src/kbech32.py
@@ -19,14 +19,23 @@ def polymod(values):
 
 def encodeAddress(prefix: str, payload: bytes, version: int):
     data = bytes([version]) + payload
-    number = int.from_bytes(data, 'big') << 1  # Round to 255 bits
+    accumulator = 0
+    data = [int(x) for x in data]
     ret = []
-    th = (1 << 5) - 1
-    for i in range(len(data)*8//5 + 1):
-        ret.append(number & th)
-        number >>= 5
-    
-    address = bytes(ret[::-1])
+    mask = (1 << 5) - 1
+    bits = 0
+    for value in data:
+        accumulator = (accumulator << 8) | value
+        bits = bits + 8
+
+        while bits >= 5:
+            bits = bits - 5
+            ret.append((accumulator >> bits) & mask)
+
+    if bits > 0:
+        ret.append((accumulator << (5 - bits)) & mask)
+
+    address = bytes(ret[::])
     checksum_num = polymod(
         bytes([ord(c) & 0x1f for c in prefix]) + 
         bytes([0]) + address  +
@@ -39,5 +48,19 @@ def toAddress(script):
     if script[0] == 0xaa and script[1] <= 0x76:
         return encodeAddress("kaspa", script[2:(2+script[1])], 0x08)
     if script[0] < 0x76:
-        return encodeAddress("kaspa", script[1:(1+script[0])], 0x0)
+        # Version = 1 if ECDSA, else Version = 0
+        version = 0x01 if (script[0] == 0x21 and script[34] == 0xab) else 0x00
+        return encodeAddress("kaspa", script[1:(1+script[0])], version)
     raise NotImplementedError(script.hex())
+
+# Uncomment for testing
+# if __name__ == "__main__":
+#     # Should be kaspa:qyp7xyqdshh6aylqct7x2je0pse4snep8glallgz8jppyaajz7y7qeq4x79fq4z
+#     test_ecdsa = toAddress(bytes([0x21, 0x03, 0xe3, 0x10, 0x0d, 0x85, 0xef, 0xae, 0x93, 0xe0, 0xc2, 0xfc, 0x65, 0x4b, 0x2f, 0x0c, 0x33, 0x58,
+#         0x4f, 0x21, 0x3a, 0x3f, 0xdf, 0xfd, 0x02, 0x3c, 0x82, 0x12, 0x77, 0xb2, 0x17, 0x89, 0xe0, 0x64, 0xab]))
+#     print(test_ecdsa, test_ecdsa == "kaspa:qyp7xyqdshh6aylqct7x2je0pse4snep8glallgz8jppyaajz7y7qeq4x79fq4z")
+    
+#     # Should be kaspa:qrazhptjkcvrv23xz2xm8z8sfmg6jhxvmrscn7wph4k9we5tzxedwfxf0v6f8
+#     test_schnorr = toAddress(bytes([0x20, 0xFA, 0x2B, 0x85, 0x72, 0xB6, 0x18, 0x36, 0x2A, 0x26, 0x12, 0x8D, 0xB3, 0x88, 0xF0, 0x4E, 0xD1,
+#         0xA9, 0x5C, 0xCC, 0xD8, 0xE1, 0x89, 0xF9, 0xC1, 0xBD, 0x6C, 0x57, 0x66, 0x8B, 0x11, 0xB2, 0xD7, 0xac]))
+#     print(test_schnorr, test_schnorr == "kaspa:qrazhptjkcvrv23xz2xm8z8sfmg6jhxvmrscn7wph4k9we5tzxedwfxf0v6f8")
